8a75ca3a8e92f513ed4dc7641e032d99c44221ce
Jonathan Diaz <64094787+Jonathan-0708@users.noreply.github.com>
2021-06-25 11:51:23 -0400

    Practica 1 React

---
+++ src/js/component/Jumbotron.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Jumbotron = () => (
+	<div className="jumbotron mt-1">
+		<h1 className="display-4">A warm welcome!</h1>
+		<p className="lead">
+			Deserunt quis nostrud laborum pariatur minim ad enim consectetur
+			anim. Ullamco et anim aliqua dolor. Et dolore minim culpa sunt
+			laborum nulla veniam duis cillum laborum ullamco et ut excepteur. In
+			consectetur ea mollit nulla qui ad incididunt amet dolore. Eiusmod
+			reprehenderit do elit voluptate proident magna proident esse tempor
+			ullamco officia. Ullamco et anim nulla velit.
+		</p>
+		<a className="btn btn-primary btn-lg" href="#" role="button">
+			Call to action!
+		</a>
+	</div>
+);
+
+export default Jumbotron;
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
+import Jumbotron from "./Jumbotron";
+import Card from "./Card";
+import Navbar from "./Navbar";
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div>
+				<Navbar />
+				<div className="container">
+					<Jumbotron />
+				</div>
+			</div>
+
+			<div className="container">
+				<div className="row">
+					<div className="col-sm-3">
+						<Card />
+					</div>
+					<div className="col-sm-3">
+						<Card />
+					</div>
+					<div className="col-sm-3">
+						<Card />
+					</div>
+					<div className="col-sm-3">
+						<Card />
+					</div>
+				</div>
+			</div>
+		</>
 	);
 }
